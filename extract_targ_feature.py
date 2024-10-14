6a612c2a6858a1089e482cf8c18647943f27b5cf
Karrthik Arya <karrthikarya@gmail.com>
2024-10-14 17:00:37 +0530

    checking

--- extract_targ_feature.py
+++ extract_targ_feature.py
@@ -91,6 +91,8 @@ def main(args):
     cur_batch = []
     for i, (path, key) in enumerate(input_paths):
       img = io.imread(path)
+      if image.shape[2] == 4:  
+        image = image[:, :, :3]
       print(img.shape)
       img = transform.resize(img, img_size)
       if img.ndim == 2:  
