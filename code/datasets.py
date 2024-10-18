fdf7b80ae4718573f64849a957b936264f8856af
Karrthik Arya <karrthikarya@gmail.com>
2024-10-18 22:10:39 +0530

    check images concat

--- code/datasets.py
+++ code/datasets.py
@@ -84,5 +84,5 @@ def collate_fn(batch):
         domains.append(domain)
         imgfiles.append(imgfile)
 
-    return {'image': torch.cat(images), 'question': torch.from_numpy(questions),
+    return {'image': torch.stack(images), 'question': torch.from_numpy(questions),
             'answer': torch.LongTensor(answers), 'question_length': lengths, "domain": domains, "imgfile": imgfiles}
